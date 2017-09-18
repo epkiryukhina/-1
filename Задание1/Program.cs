1bb5bc4fe21572637729de2928db4272874e8048
Xiaomi <Xiaomi@DESKTOP-02SFNU6>
2017-09-18 23:05:31 +0500

    1

--- Задание1/Program.cs
+++ Задание1/Program.cs
@@ -11,18 +11,18 @@ namespace Задание1
     {
         static void Main(string[] args)
         {
-            int n, i, j, s, k;
+            int n, i, j, s;
             long ss, m, max;
             int[,] a = new int[8, 8];
             int[] p = new int[9];
             int[] pmax = new int[9];
             string[] str = new string[9];
 
-            n = Int32.Parse(Console.ReadLine());
+            n = Int32.Parse(Console.ReadLine());//ввод кол-ва
 
             ss = 0;
 
-            for (i = 0; i < n; i++)
+            for (i = 0; i < n; i++)//ввод чисел в массив
             {
                 str = Console.ReadLine().Split(' ');
                 for (j = 0; j < n; j++)
@@ -32,7 +32,7 @@ namespace Задание1
                 }
             }
 
-            for (i = 0; i <= n; i++)
+            for (i = 0; i <= n; i++)//заполнение доп массива для хранения порядка букв 
                 p[i] = i;
 
             max = 0;
@@ -41,10 +41,10 @@ namespace Задание1
             {
                 m = 0;
 
-                for (i = 1; i <= n; i++)
+                for (i = 1; i <= n; i++)//сумма оставшихся на месте с учетом порядка букв
                     m += a[i - 1, p[i] - 1];
 
-                if (m > max)
+                if (m > max)//если сумма оставшихся на месте больше максимума
                 {
                     max = m;
 
@@ -54,7 +54,7 @@ namespace Задание1
 
                 j = n;
 
-                do
+                do//поиск первой большей перед меньшей
                 {
                     --j;
                 } while (!(p[j] < p[j + 1]));
@@ -63,16 +63,16 @@ namespace Задание1
                 {
                     i = n + 1;
 
-                    do
+                    do//поиск первой меньшей перед сохраненной
                     {
                         --i;
                     } while (!(p[i] > p[j]));
 
-                    s = p[i];
+                    s = p[i];//смена значений
                     p[i] = p[j];
                     p[j] = s;
 
-                    for (i = j + 1; i <= (n + j + 1) / 2; i++)
+                    for (i = j + 1; i <= (n + j + 1) / 2; i++)//замена значений
                     {
                         s = p[i];
                         p[i] = p[n + j + 1 - i];
@@ -81,7 +81,7 @@ namespace Задание1
                 }
             } while (j != 0);
 
-            for (i = 1; i <= n; i++)
+            for (i = 1; i <= n; i++)//вывод рез-ов
                 Console.Write((char)(pmax[i] - 1 + (int)'A'));
             
             Console.WriteLine();
